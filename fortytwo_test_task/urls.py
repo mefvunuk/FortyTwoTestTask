4365c4fe6454d97a564b743fd212fd8dde930d28
Mefisto <mefvunuk@gmail.com>
2017-03-21 21:44:10 +0200

    Ticket:1 Turn html mockup into django template

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,4 +11,5 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 )
