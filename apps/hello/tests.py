31f302a3d8729be3903815d1c9ce2be7e07297c5
Mefisto <mefvunuk@gmail.com>
2017-03-22 20:32:25 +0200

    Ticket 1: Test for the view returning hard-coded data for the template

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-
+from django.test import Client
+from django.core.urlresolvers import reverse, resolve
 # Create your tests here.
 
 
@@ -7,3 +8,17 @@ class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
+
+    def test_the_view(self):
+        """Test for the view returning hard-coded data for the template"""
+
+        found = resolve('/')
+        self.assertEqual(found.func, 'Home_view')
+
+        self.client = Client()
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Oleg', response.content)
+        self.assertIn(b'Vunuk', response.content)
+        self.assertIn(b'mefvunuk@gmail.com', response.content)
