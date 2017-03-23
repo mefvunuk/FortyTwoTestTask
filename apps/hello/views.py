172ba3b58b5e9cf100c6abd978dbb9b4a5018ead
Mefisto <mefvunuk@gmail.com>
2017-03-23 20:46:54 +0200

    deploy

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,14 +1,12 @@
 from django.views.generic.base import TemplateView
+from .models import Info
 
 
 class Home_view(TemplateView):
-    """View"""
 
     template_name = "base.html"
 
     def get_context_data(self, **kwargs):
         context = super(Home_view, self).get_context_data(**kwargs)
-        context['name'] = 'Oleg'
-        context['surname'] = 'Vunuk'
-        context['email'] = 'mefvunuk@gmail.com'
+        context['info'] = Info.objects.all()
         return context
