24fe9ce3f33b5087c930a5dedaaaf3dfaa613de9
Mefisto <mefvunuk@gmail.com>
2017-03-24 22:35:23 +0200

    Ticket 3:Test for model Request_model

---
+++ apps/request_history/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+
+class Request_model(models.Model):
+    pass
--- apps/request_history/tests.py
+++ apps/request_history/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.test import Client
 from django.core.urlresolvers import reverse, resolve
+from .models import Request_model
+from datetime import datetime
 
 
 class Request_test(TestCase):
@@ -17,3 +19,17 @@ class Request_test(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'POST', response.content)
         self.assertIn(b'http://127.0.0.1:8000/', response.content)
+
+    def test_the_model_Info(self):
+        """Test the model Request_model"""
+
+        instance = Request_model()
+        instance.method = 'POST'
+        instance.link = 'http://127.0.0.1:8000/'
+        instance.date = datetime.now()
+        instance.save()
+
+        saved_items = Request_model.objects.all()
+        first_query = saved_items[0]
+        self.assertEqual(first_query.method, "POST")
+        self.assertEqual(first_query.link, "http://127.0.0.1:8000/")
